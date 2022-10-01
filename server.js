9ab54f7d159d79ecc84b3fd4413b515907c92e20
Ogooluwa Olutimilehin <85028314+ogooluwanick@users.noreply.github.com>
2022-10-01 18:40:59 +0100

    done with error handling and learning pipe/concart

--- server.js
+++ server.js
@@ -1,1 +1,17 @@
-require("")
+let fs=require("fs")
+let concat=require("concat-stream")
+
+
+let streamFileRead = fs.createReadStream('test.txt', 'utf-8')
+
+streamFileRead.on("error",()=>{
+        console.log(error)
+        process.exit(1)
+})
+
+let addAndManageChunksData = concat( (totalText) =>{
+        console.log('There are ' + totalText.split(' ').length + ' words in the file')
+        console.log(totalText.split(' '))
+      }) // returns a writable stream
+      
+      streamFileRead.pipe(addAndManageChunksData) ;
